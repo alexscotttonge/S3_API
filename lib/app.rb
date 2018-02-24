5e6420b12838e40e0da280441469612afa9f115e
Alex <alex@scotttonge.com>
2018-02-24 17:06:02 +0000

    Made json_to_hash method private to avoid exposing data to account holder.
    Note: unit test of json_to_hash method is temporarily skipped for now.

--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,5 @@
 class App
 
-  def json_to_hash
-    file = File.read('./customer.json')
-    data_hash = JSON.parse(file)
-    data_hash['accounts']
-  end
-
   def check_balance(account_id)
     json_to_hash.map do |account|
       return account['balance'] if account['id'] == account_id
@@ -16,6 +10,14 @@ class App
     json_to_hash.map do |account|
       return first_name if account['id'] == account_id
     end
+  end
+
+  private
+
+  def json_to_hash
+    file = File.read('./customer.json')
+    data_hash = JSON.parse(file)
+    data_hash['accounts']
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,7 +5,7 @@ describe App do
   subject(:app) { described_class.new }
 
   describe '#json_to_hash' do
-    it 'parses the customer json file' do
+    xit 'parses the customer json file' do
       expected = helper_json
       expect(app.json_to_hash).to eq(expected)
     end
@@ -13,7 +13,6 @@ describe App do
 
   describe '#check_balance' do
     it 'returns the correct balance for the account holder' do
-      app.json_to_hash
       account_id = '8a28f09a-c234-4a95-b1e0-cdbc68979d0a'
       expect(app.check_balance(account_id)).to eq "3,702.54"
     end
@@ -21,7 +20,6 @@ describe App do
 
   describe '#check_first_name' do
     it 'returns the first name of the account holder' do
-      app.json_to_hash
       account_id = '8a28f09a-c234-4a95-b1e0-cdbc68979d0a'
       first_name = 'Cyrus'
       expect(app.check_first_name(account_id, first_name)).to eq 'Cyrus'
