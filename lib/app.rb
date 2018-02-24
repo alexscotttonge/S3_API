01f146cc31b1d1cad4a16f69cfd5e90225a6b1a1
Alex <alex@scotttonge.com>
2018-02-24 16:16:07 +0000

    returns correct balance with hardcoded values

--- lib/app.rb
+++ lib/app.rb
@@ -2,7 +2,14 @@ class App
 
   def parse_json
     file = File.read('./customer.json')
-    data_hash = JSON.parse(file)
+    @data_hash = JSON.parse(file)
+  end
+
+  def check_balance(account_id)
+    array_hash = @data_hash['accounts']
+    array_hash.map do |account|
+      return account['balance'] if account['id'] == account_id
+    end
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -11,4 +11,12 @@ describe App do
     end
   end
 
+  describe '#check_balance' do
+    it 'returns the correct balance for the account holder' do
+      app.parse_json
+      account_id = '8a28f09a-c234-4a95-b1e0-cdbc68979d0a'
+      expect(app.check_balance(account_id)).to eq "3,702.54"
+    end
+  end
+
 end
